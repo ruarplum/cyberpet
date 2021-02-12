f94f99a71c3cfce8217087afebc13948367dac65
Ruaraidh <rca.plummer@gmail.com>
2021-02-12 11:28:59 +0000

    #4 - Pet.js classes created - not tested

--- pets/pet.js
+++ pets/pet.js
@@ -1,29 +1,111 @@
+// Pet Object Setup 
 class Pet {
-    constructor(name, hunger, thirst, happiness, health) {
-        this.name = name;
-        this.hunger = 50;
-        this.thirst = 50;
-        this.happiness = 50;
-        this.health = 50;
+    constructor(name, play, hunger, thirst, happiness, toilet) {
+        // underscore used to keep origional values
+        this._name = name;
+        this._hunger = 50;
+        this._thirst = 50;
+        this._happiness = 50;
+        this._health = 50;
+        this._play = 50;
+        this._toilet = 0;
     }
-}
-class Dog extends Pet {
-    constructor(name, lovesMeat) {
-        super(name, hunger, thirst, happiness, health);
-        this.lovesMeat = lovesMeat;
+    // getters used (I think) to create adjustable values of properites 
+    get name() {
+        return this._name;
     }
-}
-class Cat extends Pet {
-    constructor(name, lovesFish) {
-        super(name, hunger, thirst, happiness, health);
-        this.lovesFish = lovesFish;
+    get hunger() {
+        return this._hunger;
     }
-}
-class Rabbit extends Pet {
-    constructor(name, lovesVeg) {
-        super(name, hunger, thirst, happiness, health);
-        this.lovesVeg = lovesVeg;
+    get happiness() {
+        return this._happiness;
+    }
+    get play() {
+        return this._play;
+    }
+    get health() {
+        return this._health;
+    }
+    get toilet() {
+        return this._toilet;
+    }
+
+    // Functions/ Interactions with pet and the impacts they have
+    
+    giveFood() {
+        this.hunger -= 50;
+        this.thirst += 25;
+        this.happiness += 15;
+        this.toilet += 50;
+    }
+
+    giveDrink() {
+        this.hunger += 25;
+        this.thirst -= 50;
+        this.happiness += 15;
+        this.toilet += 35;
+    }
+
+    playTime() {
+        this.hunger -= 50;
+        this.thirst -= 50;
+        this.happiness += 50;
+        this.toilet += 10;
+        this.health += 20; 
+    }
+    ignorePet() {
+        this.hunger += 15;
+        this.thirst += 20;
+        this.happiness -= 20;
+        this.toilet += 20;
+        this.health -= 15;
     }
 }
 
-const dog1 = steph
+// Subclass of pet types creation 
+
+class Dog extends Pet {
+    constructor(name, throwStick) {
+        super(name);
+        this._throwStick = throwStick;
+    }
+    throwStick() {
+        this.throwStick ++;
+        this.hunger += 10;
+        this.thirst += 10;
+        this.happiness += 25;
+        this.toilet += 10;
+        this.health += 15;
+    }
+}
+
+class Cat extends Pet {
+    constructor(name, bellyRub) {
+        super(name, bellyRub);
+        this._bellyRub = bellyRub;
+    }
+    bellyRub() {
+        this.bellyRub ++;
+        this.happiness += 20;
+        this.health += 5;
+        this.hunger += 5;
+        this.thirst += 5;
+        this.toilet += 15;
+    }
+}
+
+class Rabbit extends Pet {
+    constructor(name, stress) {
+        super(name);
+        this._stress = 10;
+    }
+    pickUp() {
+        this.stress += 50;
+        this.health += 10;
+        return console.log(`${this.name} has crapped itself from the stress`);
+    }
+}
+
+How to test?
+
+// const dog1 = steph
